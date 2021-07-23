2c21fbeea98b5c2b872f9d281293d18baf91c5e4
FatehjotSingh <72024514+FatehjotSingh@users.noreply.github.com>
2021-07-23 19:51:29 +0530

    Update P-C108.py

--- P-C108.py
+++ P-C108.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.figure_factory as pff
 
-fil= pd.read_csv('csvs/8.csv')
+fil= pd.read_csv('8.csv')
 
 grapq= pff.create_distplot([fil['Avg Rating'].tolist()],['name'],show_hist=False)
 grapq.show()
